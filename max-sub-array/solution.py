8681226aa3fbbb070ce57b5ca94844608e9304da
Danilo Lekovic <danilo@lekovic.ca>
2021-01-14 23:02:15 -0800

    Max sub array

---
+++ max-sub-array/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        curSum = maxSum = nums[0]
+        
+        for num in nums[1:]:
+            curSum = max(num, curSum + num)
+            maxSum = max(maxSum, curSum)
+            
+        return maxSum
